7e1ce0c445313ec971c6679c4a606c3452478979
Batzul Dembereldorj <batzuldmb@gmail.com>
2021-10-17 18:14:10 -0700

    Fix

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = function (app) {
     app.get('*', function (req, res) {
+        console.log(req);
         res.sendFile(path.join(__dirname, "../client/build/index.html"));
     });
 
